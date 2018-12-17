6efb7d32001e86d8ba83d250b176d0756ea66247
Pyhro36 <pylou_11@hotmail.fr>
2018-12-17 22:06:51 +0100

    td2 rearrange indexes

--- cuda_td_2.cu
+++ cuda_td_2.cu
@@ -15,11 +15,11 @@
 //@@ INSERT CODE HERE
 __global__ void colorToGrayShadesKernel(float *in, float *out, int height, int width, int channels) {
 
-    int jj = threadIdx.x + (blockDim.x * blockIdx.x);
-    int ii = threadIdx.y + (blockDim.y * blockIdx.y);
+    int ii = threadIdx.x + (blockDim.x * blockIdx.x);
+    int jj = threadIdx.y + (blockDim.y * blockIdx.y);
 
     if ((ii < height) && (jj < width)) {
-        int idx = (height * ii) + jj;
+        int idx = (ii * height) + jj;
         float r = in[channels * idx];
         float g = in[(channels * idx) + 1];
         float b = in[(channels * idx) + 2];
@@ -95,7 +95,7 @@ int main(int argc, char *argv[]) {
     {
         for (i = 0; i < imageHeight; ++i)
         {
-            static unsigned char color[3];
+            static unsigned char color[1];
             color[0] = hostOutputImageData[(i * imageHeight) + j] * 255.0;
             (void) fwrite(color, 1, 1, fp);
         }
